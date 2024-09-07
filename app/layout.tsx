b4f82a273f54a1b4eee0632a35a91ddc6a36e125
Ayush Sehrawat <69469790+AyushSehrawat@users.noreply.github.com>
2024-09-07 10:49:13 +0530

    fix: fix analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function Layout({ children }: { children: ReactNode }) {
 
                 <script
                     defer
-                    src="https://stats.ayush.gg/script.js"
+                    src="http://umami:3000/script.js"
                     data-website-id="b859b172-b345-47b2-aa4b-0576f20cee75"
                 ></script>
             </body>
